8c80630890c939544b5cac510d85421dfddb03c6
Adrian Lesniak <adrian110288@gmail.com>
2018-02-17 10:35:43 +0000

    Add Lombok. Add Bootstrap to template.

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,7 +1,10 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Notes {
 
@@ -10,21 +13,5 @@ public class Notes {
     private Long id;
     @Lob
     private String recipeNotes;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 
 }
