16278124ecc61df7151bd6d124e80cb20e688501
Adrian Lesniak <adrian.lesniak@nutmeg.com>
2018-02-23 13:42:45 +0000

    Pass exception to view.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -63,10 +63,12 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
+    public ModelAndView handleNotFound(Exception e) {
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", e);
+
         return modelAndView;
     }
 
--- src/main/java/guru/springframework/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -48,7 +48,7 @@ public class RecipeServiceImpl implements RecipeService {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()) {
-            throw new NotFoundException("Recipe not found!");
+            throw new NotFoundException("Recipe not found for Id value: " + id.toString());
         }
 
         return recipeOptional.get();
