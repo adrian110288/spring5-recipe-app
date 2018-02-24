1b1900a9a5cc560f5e1e6f091cbf401898ea7499
Adrian Lesniak <adrian110288@gmail.com>
2018-02-24 20:09:57 +0000

    Validate recipe.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -38,7 +41,17 @@ public class RecipeController {
 
     @PostMapping
     @RequestMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command,
+                               BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            bindingResult
+                    .getAllErrors()
+                    .forEach(objectError -> log.debug(objectError.toString()));
+
+            return "recipe/recipeform";
+        }
+
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
@@ -54,7 +67,6 @@ public class RecipeController {
     @GetMapping
     @RequestMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id) {
-
 
         log.debug("Deleting by id: " + id);
 
